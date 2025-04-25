cb344bfced76314776b19cfaad25852c0789c15d
Simon Rintala <siimon_7@icloud.com>
2025-04-25 09:18:30 +0200

    Added logout (postmapping), authCheck (getmapping) to AuthenticationController

--- src/main/java/com/Java24GroupProject/AirBnBPlatform/controllers/AuthenticationController.java
+++ src/main/java/com/Java24GroupProject/AirBnBPlatform/controllers/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.Java24GroupProject.AirBnBPlatform.controllers;
 import com.Java24GroupProject.AirBnBPlatform.DTOs.AuthenticationRequest;
 import com.Java24GroupProject.AirBnBPlatform.DTOs.AuthenticationResponse;
 import com.Java24GroupProject.AirBnBPlatform.DTOs.UserRequest;
+import com.Java24GroupProject.AirBnBPlatform.models.User;
 import com.Java24GroupProject.AirBnBPlatform.services.UserService;
 import com.Java24GroupProject.AirBnBPlatform.util.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,16 +12,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //only handles registration of new users and login of users only
 @RestController
@@ -93,5 +92,48 @@ public class AuthenticationController {
                     .status(HttpStatus.UNAUTHORIZED)
                     .body("Incorrect username or password");
         }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        // skapa en utgången cookie för att ersätta den befintliga jwt cookien
+        ResponseCookie jwtCookie = ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .secure(false) // VIKTIGT! ändra i production
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        // response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
+
+        // rensa securitykontexten
+        SecurityContextHolder.clearContext();
+
+        // returnera svar med utgången cookie
+        return ResponseEntity.ok()
+                // hade kunnat ta bort denna raden och använda rad 134
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body("Logout successful!");
+    }
+
+    // kolla om en user är authenticated
+    @GetMapping("/check")
+    public ResponseEntity<?> checkAuthentication() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // kontrollera om användaren är authenticated
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not authenticated!");
+        }
+
+        // returnera user info om authentication
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = userService.findByUsername(userDetails.getUsername());
+
+        return ResponseEntity.ok(new AuthenticationResponse(
+                "Authenticated",
+                user.getUsername(),
+                user.getRoles()
+        ));
     }
 }
